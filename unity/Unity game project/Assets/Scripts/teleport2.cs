93f821a5412f097c8cdffb04c761a184b3b590ba
AlvaroMartinezQ <alvaroo2302@gmail.com>
2022-05-10 12:49:49 +0200

    Added desert sound - Added tps between scenes

---
+++ unity/Unity game project/Assets/Scripts/teleport2.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class teleport2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            SceneManager.LoadScene("Scene3");
+        }    
+    }
+}
